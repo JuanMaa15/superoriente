9dad6caffc60f022963fc16460c468534c90cc73
PC Simona <simona@gmail.com>
2022-01-12 14:30:59 -0500

    Se organizó la carta laboral (backend)

--- controller/usuario/cartaLaboral.php
+++ controller/usuario/cartaLaboral.php
@@ -3,7 +3,7 @@
 require ("../../public/libs/tfpdf/tfpdf.php");
 
 require_once ("../../models/DAO/UsuarioDAO.php");
-require_once ("../../models/DTO/UsuarioDTO.php");
+require_once ("../../models/DAO/TipoContratoDAO.php");
 
 
 
@@ -15,10 +15,19 @@ $usuariodto = $usuariodao->listaUsuario($id_usuario);
 
 $genero = '';
 
-if ($usuariodto->getGenero() == "masculino") {
+if ($usuariodto->getGenero() == 1) {
     $genero = "El señor";
 }else{
     $generp = "La señora";
+}
+
+$tipoContratodao = new TipoContratoDAO();
+$listaTiposContratos = $tipoContratodao->listaTiposContratos(); 
+$tipo_contrato = '';
+for ($i=0; $i < count($listaTiposContratos); $i++) { 
+    if ($usuariodto->getTipo_contrato() == $listaTiposContratos[$i]->getId_tipo_contrato()) {
+        $tipo_contrato = $listaTiposContratos[$i]->getNombre();
+    }
 }
 
 
@@ -40,7 +49,7 @@ $pdf->Cell(0,0,'Identificado con NIT. 800.045.218-4', 0,0, 'C');
 $pdf->Ln(10);
 $pdf->Cell(0,0,'HACE CONSTAR QUE:', 0,0, 'C');
 $pdf->Ln(20);
-$pdf->MultiCell(0,5,utf8_decode( $genero  . ' '. strtoupper($usuariodto->getNombre(). " ". $usuariodto->getApellido()) .' con número de cédula ' . $usuariodto->getId_usuario() . ' labora en esta empresa con un contrato ' . $usuariodto->getTipo_contrato() . ', desde ' . $usuariodto->getFecha_inicio() . ' desempeñándose bajo el cargo de ' . strtoupper($usuariodto->getCargo()) . ', devengando un salario $' . intval($usuariodto->getSueldo()) . '.'), 'C');
+$pdf->MultiCell(0,5,utf8_decode( $genero  . ' '. strtoupper($usuariodto->getNombre(). " ". $usuariodto->getApellido()) .' con número de cédula ' . $usuariodto->getId_usuario() . ' labora en esta empresa con un contrato ' . $tipo_contrato . ', desde ' . $usuariodto->getFecha_ingreso() . ' desempeñándose bajo el cargo de ' . strtoupper($usuariodto->getCargo()) . ', devengando un salario $' . intval($usuariodto->getSalario()) . '.'), 'C');
 
 $pdf->Ln(20);
 $pdf->Cell(0,0,'Dicho certificado se expide a solicitud verbal de la interesada.', 0,0, 'L');
