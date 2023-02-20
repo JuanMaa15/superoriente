b19806ecf0ae59cec59fde15ac538e324440d59a
Juan Manuel <juanmanuel.41595@gmail.com>
2023-02-20 12:15:38 -0500

    Nuevos cambios

--- config/conexion.php
+++ config/conexion.php
@@ -10,7 +10,7 @@ class Conexion {
         $pass = "simona2021";
 
         try {
-            $cnx = new PDO("sqlsrv:server=nombre_server\DESARROLLOSOFT;database=nombre_db", $user, $pass);
+            $cnx = new PDO("sqlsrv:server=nombre_server;database=nombre_db", $user, $pass);
             return $cnx;
         } catch (Exception $e) {
             echo "Error al conectar con la base de datos ". $e->getMessage();
